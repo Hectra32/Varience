ea65639c7d25dcf12c2cc66d6951f3985a7359ad
Hectra32 <vsaddison093@gmail.com>
2026-07-17 19:47:21 +0530

    Better Spawn Position Logic

--- src/fetch.c
+++ src/fetch.c
@@ -1,5 +1,5 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+
 #include "fetch.h"
 
 void display_size(int* width, int* height)
@@ -25,13 +25,9 @@ void spawn_pos(int number, int* x, int* y)
 	int height, width;
 	display_size(&width, &height);
 
-	printf("number = %d\n", number);
-
 	int base_x = width/2;
 	int base_y = height/2;
 
-	for(int i = 0; i < number + 1; i++){
-		*x = base_x + (20 * i);
-		*y = base_y + (20 * i);
-	}
+	*x = base_x + (20 * number);
+	*y = base_y + (20 * number);	
 }
